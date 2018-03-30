6ea76add013f6da2bd0d7f760f891af4db6d6316
Amós Batista <amos.silva@gmail.com>
2018-03-30 13:44:36 -0300

    First commit

---
+++ pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return { name: 'world' }
+  },
+  head: {
+	  meta: [
+	    { charset: 'utf-8' },
+	    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+	    { name: 'og:title', content: 'Teste de página home' },
+	    { name: 'og:description', content: 'Primeira página como vue deploy' },
+	  ],
+	  link: [
+	    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
+	  ]
+	}
+}
+</script>
