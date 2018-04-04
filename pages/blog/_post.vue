af50fb12b05755e5e60e4719bf1a72f23d4ccb18
Amós Batista <amos.silva@gmail.com>
2018-04-04 20:55:44 -0300

    Cleaning post content

--- pages/blog/_post.vue
+++ pages/blog/_post.vue
@@ -8,7 +8,6 @@
     export default {
 
         asyncData: function(params){
-            console.log ("teste requisição");
             return req.get(process.env.BLOG_URL + '/posts', {
                 params: {
                     '_embed': 1,
@@ -16,41 +15,52 @@
                 }
             })
                 .then(function(res) {
-                    let content = res.data[0];
-                    let postData = {
+                    let content = res.data[0]
+
+                    let contentCleaner = function(_content){
+                        let _newContent = '';
+
+                        for(var _contentChar = 0; _contentChar < _content.length; _contentChar++){
+                            _newContent = _newContent + _content.charAt(_contentChar).replace(String.fromCharCode(160), ' ');
+                        }
+
+                        return _newContent;
+                    };
+
+                    let post = {
                         title: content.title.rendered,
                         description: content.excerpt.rendered.replace('<p>', '').replace('</p>', ''),
                         thumbnail: content._embedded["wp:featuredmedia"][0] != undefined
                             ? content._embedded["wp:featuredmedia"][0].source_url
                             : '',
-                        content: '<p>Durante a criação, <a href=\"https://medium.com/amsbatista\">criei uma conta na Medium</a>, onde comecei a contar o passo a passo de todo o processo de criação. Como hostear a página, como controlar o código-fonte usando <a href=\"https://git-scm.com/\">GIT</a>, prototipando o projeto, etc...</p>'
+                        content: contentCleaner(content.content.rendered)
                     }
-                    let metadata = {
-                        title: postData.title,
-                        description: postData.description,
-                        thumbnail: postData.thumbnail,
+                    post.meta = {
+                        title: post.title,
+                        description: post.description,
+                        thumbnail: post.thumbnail,
                         url: "/blog/",
                         type: "post"
                     };
 
-                    return {postData, metadata}
+                    return {post}
                 })
                 .catch( function (err){
-                    let postData = {
+                    let post = {
                         title: "Erro na geração do post",
                         description: "O erro é " + err,
                         url: "/",
-                        thumbnail: "/thumbnails/home.jpg",
+                        thumbnail: "/thumbnails/home.jpg"
                     }
 
-                    let metadata = {
-                        title: postData.title,
-                        description: postData.description,
-                        thumbnail: postData.thumbnail,
+                    post.meta = {
+                        title: post.title,
+                        description: post.description,
+                        thumbnail: post.thumbnail,
                         url: "/blog/",
                         type: "post"
                     }
-                    return { postData, metadata }
+                    return { post }
                 })
         },
 
@@ -68,7 +78,9 @@
 <template>
 
     <div class="container">
-        <layout :metadata="metadata" :title="postData.title" :description="postData.description" :thumbnail="postData.thumbnail" :content="postData.content" />
+        
+        <layout :metadata="post.meta" :title="post.title" :description="post.description" :thumbnail="post.thumbnail" :content="post.content" />
+        }
     </div>
 </template>
 
