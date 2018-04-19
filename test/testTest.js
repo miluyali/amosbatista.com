7bc4cc0f63eb2ad90f8e8cb1d54b5ca0d0e0e90e
Amos <amos.silva@gmail.com>
2018-04-19 19:19:43 -0300

    Mocha implement

---
+++ test/testTest.js
@@ -0,0 +1,10 @@
+//import testFunc from './myModule.js'
+
+describe('myTest', function(){
+    it('should return KKK e aeh man', function(){
+        chai.expect('testFunc.metodoParaTestar()'.to.equal('KKKK e aeh man'))
+    })
+});
+
+
+export default {}
