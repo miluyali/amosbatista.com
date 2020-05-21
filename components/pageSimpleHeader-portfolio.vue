5f6d8ed5f5b67c8ad7dea55cf3f3c0d873166a7c
Amós <amos.silva@gmail.com>
2020-05-21 20:13:46 -0300

    feat(component): header com breadcrumb de portfolio

---
+++ components/pageSimpleHeader-portfolio.vue
@@ -0,0 +1,46 @@
+<script>
+  export default {
+    data: function () {
+      return {}
+    }
+  }
+</script>
+
+
+<template lang="pug">
+
+  header.title
+
+    span.home
+      a(href="/")
+        |home 
+    span.separator
+      |\
+    span.portfolio
+      | portfolio
+</template>
+
+
+<style lang="less" scoped>
+    
+  @import "../assets/variables.less";
+  @import '../assets/objects.less';
+   
+  header {
+    font-size: 75%;
+  }
+
+  .home {
+    color: hsl(250, 85%, 45%);
+  }
+
+  .separator {
+    color: hsl(249, 45%, 65%);
+    margin: 0 8px 0 4px;
+  }
+
+  .portfolio {
+    color: hsl(250, 25%, 75%);
+  }
+
+</style>
