09a596a0b2c1e990c2426ca7fad9327bf131a5a8
Amós Batista <amos.silva@gmail.com>
2021-11-03 18:51:52 -0300

    feat: página e comp dumbing of age

---
+++ pages/dumbingofage_graph/components/graph/index.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  #graphContainer
+</template>
+
+<script>
+import vis from 'vis';
+
+export default {
+  mounted () {
+    const nodes = new vis.DataSet([
+      { id: 1, label: "Joyce", shape: "circularImage", image: "\\teste" },
+      { id: 2, label: "Sarah" },
+      { id: 3, label: "Dorothy" },
+      { id: 4, label: "Walky" },
+      { id: 5, label: "Jennifer" },
+    ]);
+
+    // create an array with edges
+    const edges = new vis.DataSet([
+      { from: 1, to: 2, value: 1 },
+      { from: 1, to: 3, value: 5 },
+      { from: 2, to: 3, value: 2 },
+      { from: 2, to: 4, value: 1 },
+      { from: 3, to: 5, value: 9 },
+    ]);
+
+    // create a network
+
+    const container = document.getElementById("graphContainer");
+    const data = {
+      nodes: nodes,
+      edges: edges,
+    };
+    const options = { 
+      width: "100%",
+      height: "500px",
+      autoResize: true,
+    };
+    new vis.Network(container, data, options);
+  }
+}
+</script>
+
+<style>
+  #graphContainer {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ pages/dumbingofage_graph/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .doa
+    h1
+      | Dumbing of Age - The graph
+    graph-component
+</template>
+
+<script>
+import graphComponent from './components/graph';
+
+export default {
+  components: { graphComponent }
+}
+</script>
+
+<style>
+  graph-component {
+    width: 100%;
+  }
+</style>
