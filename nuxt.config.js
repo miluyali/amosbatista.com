f55115504e8fccb4da83d4696933fe14e375c624
Amós Batista <amos.silva@gmail.com>
2021-11-11 22:52:31 -0300

    fix: removido config de host

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
 export default {
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
   buildModules: ['@nuxt/typescript-build'],
   modules: [
     // Simple usage
