d96aea67340b2306dd0ec34d1bacee7dc4c20f24
Amós Batista <amos.silva@gmail.com>
2021-11-14 23:31:57 -0300

    feat: new home

--- pages/index.vue
+++ pages/index.vue
@@ -42,9 +42,19 @@
     link-style
     
     .full-page
-      a.link-image.me(href="#")
-      a.link-image.censurador(href="/censurador")
-      a.link-image.git(href="https://github.com/amosbatista")
+      a.link-image(href="#")
+        .bg.me
+        p.text-inside amosBatista.com
+      a.link-image(href="/dumbingofage_graph")
+        .bg.doa
+        p.text-inside dumbing of age - graph
+      a.link-image(href="/censurador")
+        .bg.censurador
+        p.text-inside censure minha música
+      a.link-image(href="https://github.com/amosbatista")
+        .bg.git
+        p.text-inside github
+        
     //--.middle
       h1 Amós Batista
       a.link(href="/censurador") Censure minha música
@@ -58,11 +68,14 @@
 
 
 <style lang="less">
+  
   @import '../assets/generic.less';
   @import '../assets/variables.less';
   @import '../assets/mixin.less';
   @import '../assets/objects.less';
   @import '../assets/base.less';
+  
+  @imagePercentage: 25%;
 
   .home-page {
     display: flex;
@@ -75,12 +88,60 @@
     width: 100vw;
     height: 100vh; 
     overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    
+    @media (orientation: portrait) {
+      flex-direction: column;
+    }
     
     .link-image {
-      display: inline-block;
-      width: 33%;
+      width: @imagePercentage;
       height: 100vh;
       background-position: center;
+      position: relative;
+      
+      display: flex;
+      align-content: center;
+      align-items: center;
+      flex-direction: row;
+      
+      &:hover .bg {
+        filter: blur(5px) grayscale(0.5) brightness(0.4);  
+      }
+      
+      &:hover .text-inside {
+        filter: blur(0px);
+      }
+      
+      @media (orientation: portrait) {
+        width: 100%;
+      }
+    }
+    .text-inside, .bg {
+      transition: filter 0.4s ease;
+    }
+    .text-inside {
+      font-family: @base-font;
+      color: @color-terciary;
+      filter: blur(5px);
+      font-size: 110%;
+      text-decoration: none;
+      margin: 0;
+      display: inline-block;
+      
+      &:visited {
+        text-decoration: none;
+      }
+    }
+    
+    .bg {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -5;
     }
     
     .me {
@@ -95,6 +156,13 @@
     
     .git {
       background-image: url('../static/Octocat.jpg');
+      background-position: center;
+    }
+    
+    .doa {
+      background-image: url('../static/doa_graph.png');
+      background-size: cover;
+      background-position: center;
     }
   }
 
